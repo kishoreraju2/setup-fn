c85f94688c786282332ee3cd7ff11bd66ab5976d
KIRAJU <KIRAJU@KIRAJU-IN.in.oracle.com>
2020-08-04 14:19:36 +0530

    removed sh

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 const tc = require('@actions/tool-cache');
 
-try {
+async function installFn() {
   const downloadUrl = "https://raw.githubusercontent.com/fnproject/cli/master/install"
   core.info(`Download from "${downloadUrl}"`)
   const pythonPath = tc.downloadTool(downloadUrl);
-  core.setOutput(`${pythonPath}`)
-  exec.exec('sh',['curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh'])
+  core.info(`${pythonPath}`)
+}
+
+async function run() {
+  await installFn();
+}
+
+run();
+
+try {
+  
+  core.info(`${pythonPath}`)
+  exec.exec('sh',['curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install'])
   core.setOutput("Successfully installed Fn");
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
