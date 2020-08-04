bbf3ee00a2a137528027455fed033173a95032d8
KIRAJU <KIRAJU@KIRAJU-IN.in.oracle.com>
2020-08-04 13:57:56 +0530

    add cache

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
 
 try {
-  exec.exec('bash',['curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh'])
+  const downloadUrl = "https://raw.githubusercontent.com/fnproject/cli/master/install"
+  core.info(`Download from "${downloadUrl}"`)
+  const pythonPath = await tc.downloadTool(downloadUrl);
+  core.info(`${pythonpath}`)
+  exec.exec('sh',['curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh'])
   core.setOutput("Successfully installed Fn");
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
